45cc4d0e873d7fba7e4f33f72eb719f221d34d4f
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2023-02-01 07:31:34 +0530

    Added actions, reducers and store

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import User from "./User";
 function App() {
   return (
     <div className="App">
-       <h2> Learn Redux</h2>
-      <User 
-      data ={{
-        name:"Ajinkya",
-        designation:"Jr Software Developer"
-      }}
-        />
+      <h2> Learn Redux</h2>
+      <User
+        data={{
+          name: "Ajinkya",
+          designation: "Jr Software Developer",
+        }}
+      />
+
+      <div>
+        <a className="minus"> <span>-</span></a>
+        <input type="text" value="0" />
+        <a className="plus"><span>+</span></a>
+      </div>
     </div>
   );
 }
---
+++ src/services/Reducers/index.js
@@ -0,0 +1,8 @@
+import { changeTheNumberReducerFunction } from "./reducer";
+import {combineReducers} from 'redux';
+
+const rootReducer = combineReducers({
+    changeTheNumberReducerFunction
+})
+
+export default rootReducer;
